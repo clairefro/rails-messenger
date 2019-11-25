52fdab465e9aa9a2a2429d6129d883cd5f68a3af
claire froelich <claire.froelich@gmail.com>
2019-11-24 20:12:49 -0500

    chat with ajax working, but needs listener for fetching partner's new messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,11 +3,18 @@ class MessagesController < ApplicationController
     @conversation = Conversation.includes(:recipient).find(params[:conversation_id])
     @message = @conversation.messages.create(message_params)
 
-    redirect_to conversation_path(@conversation)
-
-    # respond_to do |format|
-    #   format.js
-    # end
+    if @message.save
+      respond_to do |format|
+        format.html { redirect_to conversation_path(@conversation) }
+        format.js  # <-- will render `app/views/reviews/create.js.erb`
+      end
+    else
+      respond_to do |format|
+        format.html { render 'conversations/show' }
+        format.js  # <-- idem
+      end
+    end
+    # redirect_to conversation_path(@conversation)
   end
 
   private
